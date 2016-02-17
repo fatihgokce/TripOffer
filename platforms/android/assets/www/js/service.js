434b174299264424a033e1c993107e7c54f65530
fatih.gokce <fatihgokce07@gmail.com>
2016-02-17 18:13:04 +0200

    -isteklerin cogu tamamlandi

--- platforms/android/assets/www/js/service.js
+++ platforms/android/assets/www/js/service.js
@@ -1,10 +1,10 @@
 angular.module('ionicApp.services', [])
 .factory('treePageService', function ($http, $q) {
     return {
-        getPathBy:function(food,his,fun,latlng){
+        getPathBy:function(food,his,fun,latlng,distance){
             //(string food,string his,string fun,string latlng)
                var deferred = $q.defer();          
-            $http.get('http://tripoffer.net/api/values?food='+food+"&his="+his+"&fun="+fun+"&latlng="+latlng).success(deferred.resolve).error(deferred.reject);
+            $http.get('http://tripoffer.net/api/values?food='+food+"&his="+his+"&fun="+fun+"&latlng="+latlng+"&distance="+distance).success(deferred.resolve).error(deferred.reject);
             return deferred.promise;
         },
         getPaths: function (latlng) {
