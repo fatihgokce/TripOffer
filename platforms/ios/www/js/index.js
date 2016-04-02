3474c62ca8b38d77e166f6779bac285d2cfd1bb0
fatih <fatih@fatihs-MacBook-Air.local>
2016-04-02 21:44:03 +0300

    ios

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -18,25 +18,29 @@
  */
 var app = {
     // Application Constructor
-    initialize: function() {
+    deviceId: '',
+    initialize: function () {
         this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+    onDeviceReady: function () {
+        alert(device.uuid);
+        app.deviceId = device.uuid;
+       // alert(device.uuid);
+        //app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent: function (id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
